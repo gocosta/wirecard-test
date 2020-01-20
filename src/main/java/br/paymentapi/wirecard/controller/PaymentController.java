9594c87542d67f1c09fac3d0c929ae152baa2b7f
Guilherme Costa <guilhermeoliveira63@gmail.com>
2020-01-20 02:06:25 -0300

    Update some classes and create the rest API

--- src/main/java/br/paymentapi/wirecard/controller/PaymentController.java
+++ src/main/java/br/paymentapi/wirecard/controller/PaymentController.java
@@ -1,13 +1,15 @@
 package br.paymentapi.wirecard.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
 
-    @PostMapping(value = "/pay")
+    @PostMapping("/pay")
     public String pay(){
         System.out.println("doneee!!!!");
         return "done";
